6a096ff37e7e9e1e55e6e3f430b27226f4e15edf
guruchara <gurucharanchouahn7@gmail.com>
2023-01-30 01:17:30 +0530

    add and fix api

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 
 const express = require('express')
 const cors = require('cors')
+const path = require('path');
+const nodemailer = require('nodemailer')
 
 const mongoose = require('mongoose')
 const app = express();
@@ -9,7 +11,29 @@ mongoose.connect('mongodb://127.0.0.1/userInfo')
     .then(() => console.log("connected")).catch(err => console.log("error", err))
 
 app.use(cors());
-app.use(express.json());
+// app.use(express.json());
+
+
+
+
+const multer = require('multer');
+const upload = multer({ dest: 'uploads/' });
+
+// const transporter = nodemailer.createTransport({
+//     host: "smtp.example.com",
+//     port: 587,
+//     secure: false,
+//     auth: {
+//         user: "email@example.com",
+//         pass: "password"
+//     }
+// });
+
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use('/uploads', express.static('uploads'))
 
 const studentInfo = new mongoose.Schema({
     name: String,
@@ -24,46 +48,64 @@ const studentInfo = new mongoose.Schema({
         },
     },
     batchYear: Number,
-    companyName:String
-    // profilePhoto:
+    companyName: String,
+    imageUrl: String
 })
 
 const UserDetails = mongoose.model('userDetails', studentInfo)
 
-app.post('/addUserInfo', async (req, res) => {
+const fs = require('fs');
 
-    const { data } = req.body
+app.get('/getSingleUser', async (req, res) => {
+    UserDetails.find({}, function (err, datas) {
+        if (err) {
+            console.error(err);
+        } else {
 
-    console.log("data37",data)
+            console.log("datas78", datas);
+            res.json({ data: datas, message: 'success 200' })
+        }
+    });
+
+})
+
+app.post('/addUserInfo', upload.single('image'), async (req, res) => {
+    const { email, companyName, url, name, batchYear } = req.body;
+    console.log('Email:', email);
+    console.log('Image:', req.file);
+    console.log('companyName', companyName)
+    console.log("url", url)
+    console.log('name', name)
+    console.log('batchYear', batchYear)
+
+    const imgUrl = req.file.path
+
+    if (!imgUrl) {
+        res.json({ message: 'bad request' })
+    }
 
     const userDataObj = new UserDetails({
-           name:data.name,
-           email:data.email,
-           companyName:data.companyName,
-           linkedinUrl:data.url,
-           batchYear:data.batchYear
+        name: name,
+        email: email,
+        companyName: companyName,
+        linkedinUrl: url,
+        batchYear: batchYear,
+        imageUrl: imgUrl,
     })
 
     const dataRes = await userDataObj.save();
 
-    console.log("empRes38", dataRes)
-    res.json({ message: "success ...." })
+    if (dataRes) {
 
-    // console.log("data29", data)
+        res.json({ message: 'success' })
+    }
+    // else
+    // {
+    //     res.json({err:error})
+    // }
+
+
 })
-
-// app.post('/search', async (req, res) => {
-
-//     const { phoneNumber } = req.body;
-
-//     console.log("Number46", phoneNumber)
-
-//     const searchData = await EmployeeSchema.find({ phoneNumber: phoneNumber })
-
-//     console.log("searchData46", searchData)
-
-//     res.json({ data: searchData, message: "successfully API hit " })
-// })
 
 app.listen(8000, () => {
     console.log(`Server is running on port 8000.`);
